aead7c4d4886855f4ea0dc6273c2db8c72e730a2
Marthin Lachira <mrthnx96@gmail.com>
2024-10-02 00:29:36 -0500

    feat: create astro pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,65 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import AstroPWA from "@vite-pwa/astro";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    tailwind(),
+    AstroPWA({
+      mode: "development",
+      base: "/",
+      scope: "/",
+      includeAssets: ["favicon.svg"],
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Astro PWA",
+        short_name: "Astro PWA",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "corelusa.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "corelusa.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "corelusa.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+      workbox: {
+        navigateFallback: "/",
+        globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"],
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.mode === "navigate",
+            handler: "NetworkFirst", // Usa NetworkFirst o CacheFirst según tus necesidades
+            options: {
+              cacheName: "pages-cache",
+              expiration: {
+                maxEntries: 50, // Define cuántas páginas quieres cachear
+              },
+            },
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true,
+        navigateFallbackAllowlist: [/^\//],
+      },
+      experimental: {
+        directoryAndTrailingSlashHandler: true,
+      },
+    }),
+  ],
+});
+

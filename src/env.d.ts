b9b3463c3bf9de5a372b9fc91a3c62fd166b1dca
Marthin Lachira <mrthnx96@gmail.com>
2024-10-04 21:44:12 -0500

    feat: sidebar with plants

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,36 @@
 /// <reference path="../.astro/types.d.ts" />
+
+export interface Plant {
+  id: number;
+  code: string;
+  description: string;
+  areas: Area[];
+}
+
+interface Area {
+  id: number;
+  code: string;
+  description: string;
+  systems: System[];
+}
+
+interface System {
+  id: number;
+  code: string;
+  description: string;
+  mawois: Mawoi[];
+}
+
+interface Mawoi {
+  id: number;
+  code: string;
+  description: string;
+  points: Point[];
+}
+
+interface Point {
+  id: number;
+  code: string;
+  description: string;
+}
+

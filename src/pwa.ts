2803f3e59288e1616b009131bebfba832db2bb4d
Marthin Lachira <mrthnx96@gmail.com>
2024-10-17 17:14:04 -0500

    fix: restore pwa

--- src/pwa.ts
+++ src/pwa.ts
@@ -1,21 +1,21 @@
-// import { registerSW } from "virtual:pwa-register";
-//
-// const updateSW = registerSW({
-//   immediate: true,
-//   onNeedRefresh() {
-//     const confirmReload = confirm(
-//       "Hay una nueva versión disponible. ¿Quieres actualizar?",
-//     );
-//     if (confirmReload) {
-//       updateSW(); // Forzar la actualización
-//     }
-//   },
-//   onRegisteredSW(swScriptUrl) {
-//     // eslint-disable-next-line no-console
-//     console.log("SW registered: ", swScriptUrl);
-//   },
-//   onOfflineReady() {
-//     // eslint-disable-next-line no-console
-//     console.log("PWA application ready to work offline");
-//   },
-// });
+import { registerSW } from "virtual:pwa-register";
+
+const updateSW = registerSW({
+  immediate: true,
+  onNeedRefresh() {
+    const confirmReload = confirm(
+      "Hay una nueva versión disponible. ¿Quieres actualizar?",
+    );
+    if (confirmReload) {
+      updateSW(); // Forzar la actualización
+    }
+  },
+  onRegisteredSW(swScriptUrl) {
+    // eslint-disable-next-line no-console
+    console.log("SW registered: ", swScriptUrl);
+  },
+  onOfflineReady() {
+    // eslint-disable-next-line no-console
+    console.log("PWA application ready to work offline");
+  },
+});

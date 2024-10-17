0976003b24e89e2cae9abdf7c64bd3a70b0e82ec
Marthin Lachira <mrthnx96@gmail.com>
2024-10-17 17:00:33 -0500

    feat: theme gray

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,68 +1,68 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import AstroPWA from "@vite-pwa/astro";
+// import AstroPWA from "@vite-pwa/astro";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
-    AstroPWA({
-      base: "/",
-      scope: "/",
-      includeAssets: ["favicon.svg"],
-      registerType: "autoUpdate",
-      injectRegister: "inline",
-      manifest: {
-        name: "Astro PWA",
-        short_name: "Astro PWA",
-        theme_color: "#ffffff",
-        icons: [
-          {
-            src: "corelusa.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "corelusa.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-          {
-            src: "corelusa.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any maskable",
-          },
-        ],
-      },
-      workbox: {
-        maximumFileSizeToCacheInBytes: 5000000,
-        cleanupOutdatedCaches: true,
-        skipWaiting: true,
-        clientsClaim: true,
-        navigateFallback: "/",
-        globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"],
-        runtimeCaching: [
-          {
-            urlPattern: ({ request }) => request.mode === "navigate",
-            handler: "NetworkFirst", // Usa NetworkFirst o CacheFirst según tus necesidades
-            options: {
-              cacheName: "pages-cache",
-              expiration: {
-                maxEntries: 50, // Define cuántas páginas quieres cachear
-              },
-            },
-          },
-        ],
-      },
-      devOptions: {
-        enabled: true,
-        navigateFallbackAllowlist: [/^\//],
-      },
-      experimental: {
-        directoryAndTrailingSlashHandler: true,
-      },
-    }),
+    // AstroPWA({
+    //   base: "/",
+    //   scope: "/",
+    //   includeAssets: ["favicon.svg"],
+    //   registerType: "autoUpdate",
+    //   injectRegister: "inline",
+    //   manifest: {
+    //     name: "Astro PWA",
+    //     short_name: "Astro PWA",
+    //     theme_color: "#ffffff",
+    //     icons: [
+    //       {
+    //         src: "corelusa.png",
+    //         sizes: "192x192",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "corelusa.png",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "corelusa.png",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //         purpose: "any maskable",
+    //       },
+    //     ],
+    //   },
+    //   workbox: {
+    //     maximumFileSizeToCacheInBytes: 5000000,
+    //     cleanupOutdatedCaches: true,
+    //     skipWaiting: true,
+    //     clientsClaim: true,
+    //     navigateFallback: "/",
+    //     globPatterns: ["**/*.{css,js,html,svg,png,ico,txt}"],
+    //     runtimeCaching: [
+    //       {
+    //         urlPattern: ({ request }) => request.mode === "navigate",
+    //         handler: "NetworkFirst", // Usa NetworkFirst o CacheFirst según tus necesidades
+    //         options: {
+    //           cacheName: "pages-cache",
+    //           expiration: {
+    //             maxEntries: 50, // Define cuántas páginas quieres cachear
+    //           },
+    //         },
+    //       },
+    //     ],
+    //   },
+    //   devOptions: {
+    //     enabled: true,
+    //     navigateFallbackAllowlist: [/^\//],
+    //   },
+    //   experimental: {
+    //     directoryAndTrailingSlashHandler: true,
+    //   },
+    // }),
   ],
 });

6aec4124016edecfef10820f20ff90a242d98215
Marthin Lachira <mrthnx96@gmail.com>
2024-11-28 21:33:44 -0500

    fix: default values system config

--- public/js/store/system-config.js
+++ public/js/store/system-config.js
@@ -1,7 +1,13 @@
 function getSystemConfigStore() {
   const config = localStorage.getItem("systemConfig");
   if (!config) {
-    return null;
+    return {
+      amplitude: 2,
+      acc_type: 2,
+      vel_type: 2,
+      dis_type: 2,
+      frequency: 2,
+    };
   }
   return JSON.parse(config);
 }

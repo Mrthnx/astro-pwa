38ae9fd4b73d2f576d94f318ada8711c7260bdad
Marthin Lachira <mrthnx96@gmail.com>
2025-05-17 23:35:13 -0500

    feat: new collect data with wifi

---
+++ public/js/fetch.js
@@ -0,0 +1,8 @@
+function fetchData(url) {
+  return fetch(url, options).then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+  });
+}

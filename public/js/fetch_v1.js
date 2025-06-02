103d0dcce7d4c1a28be4f988ccf7e87a1e84f14b
Marthin Lachira <mrthnx96@gmail.com>
2025-06-01 22:55:46 -0500

    feat: logs fetch

--- public/js/fetch_v1.js
+++ public/js/fetch_v1.js
@@ -1,4 +1,5 @@
 function fetchData(url, isJson) {
+  console.log("fetchData", url);
   return fetch(url, options).then((response) => {
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);

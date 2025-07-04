f37941df412f0ce5e694ac7c7c3c93b68d7bfc5a
Marthin Lachira <proveedor311@culqi.com>
2025-07-03 22:00:33 -0500

    feat: restore spectra

--- public/js/math-functions_v1.js
+++ public/js/math-functions_v1.js
@@ -198,7 +198,7 @@ function processWaveform(arr) {
 
 function processSpectra(arr, fixedMultiplier) {
   fixedMultiplier =
-    (fixedMultiplier ?? 1) * PARAMS_SPEC.FIXED * 1.14 * 2.136 * 1.59 * 0.9166;
+    (fixedMultiplier ?? 1) * PARAMS_SPEC.FIXED * 1.14 * 2.136 * 1.59;
   // Aplicar la condición de umbral y multiplicador
   // 0.0000085714 * 1.14 * 2.136 = 0,000020872 ACC
   // 0.0000085714 * 1.14 * 25 = 0,000244285 VEL
